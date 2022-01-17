b2b4761d5f73820dbe9c64fd97ce7697ee0c022f
Tyler Howell <tshowell1993@gmail.com>
2022-01-17 01:00:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ main = Tk()
 main.title("My 3D Print Costs")
 main.geometry("600x400")
 
+
 # Main Headings
 title = Label(main, text="My 3D Print Costs", font=("times", 28, "bold"))
 mat = Label(main, text="Material Costs", font=("times", 18, "bold"))
@@ -21,10 +22,13 @@ modWeight_field = Entry(main)
 
 # Labor Costs
 priPrep = Label(main, text="Printer Preparation (time): ")
+priPrep_field = Entry(main)
 postProcess = Label(main, text="Postprocessing (time): ")
+postProcess_field = Entry(main)
 
 # Margin
 percentMaterial = Label(main, text="Percent of Material Cost (%): ")
+percentMaterial_field = Entry(main)
 
 # buttons
 calc = Button(main, text="Calculate")
@@ -32,7 +36,7 @@ calc = Button(main, text="Calculate")
 # Widget Position Adjustments
 title.grid(row=1, column=1)
 
-mat.grid(row=2,column=0)
+mat.grid(row=2, column=0)
 filPrice.grid(row=3, column=0)
 filPrice_field.grid(row=3, column=1)
 filWeight.grid(row=4, column=0)
@@ -42,10 +46,13 @@ modWeight_field.grid(row=5, column=1)
 
 lab.grid(row=6, column=0)
 priPrep.grid(row=7)
+priPrep_field.grid(row=7, column=1)
 postProcess.grid(row=8)
+postProcess_field.grid(row=8, column=1)
 
 Mar.grid(row=9, column=0)
 percentMaterial.grid(row=10)
+percentMaterial_field.grid(row=10, column=1)
 
 calc.grid(row=11, column=1)
 
